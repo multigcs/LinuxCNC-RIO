ba04e91a71d46004971f891a6ee55b44e0b0a284
Oliver Dippel <o.dippel@gmx.de>
2023-06-23 23:35:57 +0200

    fixing index plugin

--- plugins/vin_quadencoderz/vin_quadencoderz.v
+++ plugins/vin_quadencoderz/vin_quadencoderz.v
@@ -6,8 +6,8 @@ module vin_quadencoderz
          input quadA,
          input quadB,
          input quadZ,
-         input reset_in,
-         output reg reset_out = 0,
+         input index_enable,
+         output reg index_out = 0,
          output [BITS-1:0] pos
      );
     reg [2:0] quadA_delayed;
@@ -19,23 +19,25 @@ module vin_quadencoderz
     wire count_enable = quadA_delayed[1] ^ quadA_delayed[2] ^ quadB_delayed[1] ^ quadB_delayed[2];
     wire count_direction = quadA_delayed[1] ^ quadB_delayed[2];
     reg [BITS-1:0] count = 0;
+    reg index_wait = 0;
     assign pos = count;
     always @(posedge clk) begin
-        if (reset_in == 1 && reset_out == 0) begin
-            if (quadZ_delayed == 1) begin
-                reset_out <= 1;
-                count <= 0;
-            end
+        if (index_enable == 1 && index_out == 1 && quadZ_delayed == 1) begin
+            index_out <= 0;
+            count <= 0;
+            index_wait <= 1;
         end else begin
+            if (index_enable == 1 && index_wait == 0 && index_out == 0) begin
+                index_out <= 1;
+            end else if (index_enable == 0 && index_wait == 1) begin
+                index_wait <= 0;
+            end
             if (count_enable) begin
                 if(count_direction) begin
                     count <= count + 1;
                 end else begin
                     count <= count - 1;
                 end
-            end
-            if (reset_in == 0 && reset_out == 1) begin
-                reset_out <= 0;
             end
         end
 

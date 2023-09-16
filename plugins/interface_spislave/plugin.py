0dd609e41d2c6582606867b890d08f680a0fe750
Oliver Dippel <o.dippel@gmx.de>
2023-09-16 16:00:33 +0200

    check interface type in spi plugin

--- plugins/interface_spislave/plugin.py
+++ plugins/interface_spislave/plugin.py
@@ -61,18 +61,19 @@ class Plugin:
     def funcs(self):
         func_out = []
         for num, interface in enumerate(self.jdata.get("interface", [])):
-            func_out.append(
-                f"    interface_spislave #(BUFFER_SIZE, 32'h74697277, 32'd{int(self.jdata['clock']['speed']) // 4}) spi1 ("
-            )
-            func_out.append("        .clk (sysclk),")
-            func_out.append("        .SPI_SCK (INTERFACE_SPI_SCK),")
-            func_out.append("        .SPI_SSEL (INTERFACE_SPI_SSEL),")
-            func_out.append("        .SPI_MOSI (INTERFACE_SPI_MOSI),")
-            func_out.append("        .SPI_MISO (INTERFACE_SPI_MISO),")
-            func_out.append("        .rx_data (rx_data),")
-            func_out.append("        .tx_data (tx_data),")
-            func_out.append("        .pkg_timeout (INTERFACE_TIMEOUT)")
-            func_out.append("    );")
+            if interface["type"] == "spi":
+                func_out.append(
+                    f"    interface_spislave #(BUFFER_SIZE, 32'h74697277, 32'd{int(self.jdata['clock']['speed']) // 4}) spi1 ("
+                )
+                func_out.append("        .clk (sysclk),")
+                func_out.append("        .SPI_SCK (INTERFACE_SPI_SCK),")
+                func_out.append("        .SPI_SSEL (INTERFACE_SPI_SSEL),")
+                func_out.append("        .SPI_MOSI (INTERFACE_SPI_MOSI),")
+                func_out.append("        .SPI_MISO (INTERFACE_SPI_MISO),")
+                func_out.append("        .rx_data (rx_data),")
+                func_out.append("        .tx_data (tx_data),")
+                func_out.append("        .pkg_timeout (INTERFACE_TIMEOUT)")
+                func_out.append("    );")
         return func_out
 
     def ips(self):

6c48a366be3ec3ec578cb4a54ca09fb8745f6dd4
Oliver Dippel <odippel@ypsilon.net>
2023-10-24 17:43:47 +0200

    adding graphviz output for testing

--- generators/firmware/firmware.py
+++ generators/firmware/firmware.py
@@ -2,101 +2,17 @@ import sys
 import os
 from .buildsys import *
 from .testbench import testbench
+from .flowchart import flowchart
 
-try:
-    import graphviz
-    g = graphviz.Digraph('G', filename=f'flow_rio.gv')
-    g.attr(rankdir='LR')
-
-except Exception:
-    g = None
 
 def verilog_top(project):
     top_arguments = []
-
-    if g:
-        label = f"<<table BORDER='0' CELLBORDER='1' CELLSPACING='0'>"
-        label += f"<tr><td>name</td><td>pin</td><td>dir</td></tr>"
-
-    pindirs = {}
     for pname in sorted(list(project["pinlists"])):
         pins = project["pinlists"][pname]
         for pin in pins:
-            if g:
-                label += f"<tr><td>{pin[0]}</td><td>{pin[1]}</td><td PORT=\"{pin[1]}\">{pin[2]}</td></tr>"
-                pindirs[pin[1]] = pin[2]
             if pin[1].startswith("EXPANSION"):
                 continue
             top_arguments.append(f"{pin[2].lower()} {pin[0]}")
-
-    if g:
-        label += f"</table>>"
-        g.node(f"pins", shape='plaintext', label=label)
-
-        uniqParts = {}
-        for ptype in {"jointnames", "voutnames", "vinnames", "doutnames", "dinnames"}:
-            if project[ptype]:
-                for data in project[ptype]:
-                    pid = data.get('pid') or data.get('_name')
-                    if pid not in uniqParts:
-                        uniqParts[pid] = []
-                    uniqParts[pid].append(data)
-
-
-        label = f"<<table BORDER='0' CELLBORDER='1' CELLSPACING='0'>"
-        label += f"<tr><td>name</td><td>hal</td></tr>"
-        for pid, datalist in uniqParts.items():
-            data = datalist[0]
-            for ndata in datalist:
-                if net := ndata.get("net"):
-                    label += f"<tr><td PORT=\"{net}\">net</td><td>{net}</td></tr>"
-                else:
-                    label += f"<tr><td PORT=\"rio.{ndata['_name']}\">signal</td><td>rio.{ndata['_name']}</td></tr>"
-        label += f"</table>>"
-        g.node("hal", shape='plaintext', label=label)
-
-
-        for pid, datalist in uniqParts.items():
-            data = datalist[0]
-            label = f"<<table BORDER='0' CELLBORDER='1' CELLSPACING='0'>"
-
-
-            label += f"<tr><td>plugin</td><td>{data['type']}</td></tr>"
-            for ndata in datalist:
-                label += f"<tr><td>name</td><td PORT=\"{ndata['_name']}\">{ndata['_name']}</td></tr>"
-                if net := ndata.get("net"):
-                    g.edge(f"{pid}:{ndata['_name']}", f"hal:{net}")
-                else:
-                    g.edge(f"{pid}:{ndata['_name']}", f"hal:rio.{ndata['_name']}")
-
-            if "pin" in data:
-                pin = data["pin"]
-                label += f"<tr><td PORT=\"{pin}\">pin</td><td>{pin}</td></tr>"
-
-                pindir = pindirs.get(pin)
-                if pindir == "OUTPUT":
-                    g.edge(f"pins:{pin}", f"{pid}:{pin}", dir="back")
-                elif pindir == "INOUT":
-                    g.edge(f"pins:{pin}", f"{pid}:{pin}", dir="both")
-                else:
-                    g.edge(f"pins:{pin}", f"{pid}:{pin}", dir="forward")
-
-
-            elif "pins" in data:
-                for pname, pin in data["pins"].items():
-                    label += f"<tr><td PORT=\"{pin}\">pin-{pname}</td><td>{pin}</td></tr>"
-                    pindir = pindirs.get(pin)
-                    if pindir == "OUTPUT":
-                        g.edge(f"pins:{pin}", f"{pid}:{pin}", dir="back")
-                    elif pindir == "INOUT":
-                        g.edge(f"pins:{pin}", f"{pid}:{pin}", dir="both")
-                    else:
-                        g.edge(f"pins:{pin}", f"{pid}:{pin}", dir="forward")
-            label += f"</table>>"
-            g.node(pid, shape='plaintext', label=label, href=f"https://github.com/multigcs/LinuxCNC-RIO/tree/main/plugins/{data['type']}")
-
-        g.view()
-
 
     top_data = []
     top_data.append("/*")
@@ -404,6 +320,7 @@ def generate(project):
 
     verilog_top(project)
     testbench(project)
+    flowchart(project)
 
 
     # build files (makefiles/scripts/projects)

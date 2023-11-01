e8c6536b662b19ae70ec5c5585ed9a7a45ec882e
Oliver Dippel <odippel@ypsilon.net>
2023-11-01 14:36:34 +0100

    better parser / docgen

--- files/verilog2md.py
+++ files/verilog2md.py
@@ -5,86 +5,102 @@ import pprint
 import re
 
 
-patternModule = re.compile(r"module\s+(?P<name>\w+)(?P<params>\s*#\([^\)]*\))?\s\((?P<args>[^\)]*)\)\s*;(?P<data>[\s\S]*?(?=endmodule))endmodule")
+patternModule = re.compile(r"module\s+(?P<name>\w+)(?P<params>\s*#\([^\)]*\))?\s*\((?P<args>[^\)]*)\)\s*;(?P<data>[\s\S]*?(?=endmodule))endmodule")
 patternParam = re.compile(r"parameter(?P<type>\s+[a-zA-Z]+)?(?P<size>\s*\[.*\])?(?P<name>\s+\w+)\s+(?P<default>=.*)")
 patternArg = re.compile(r"(?P<dir>output|input|inout)?(?P<type>\s+reg|wire)?(?P<size>\s\[[^\]]*\])?(?P<name>\s\w+)")
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "verilog", help="verilog file", type=str, default=None
-)
-args = parser.parse_args()
-
-verilogData = open(args.verilog, "r").read()
-
-verilogData = re.sub(r"//.*", "", verilogData)
-#verilogData = re.sub(r"`(if|else|end|def).*", "", verilogData)
-verilogData = re.sub(r"/\*.*\*/", "", verilogData)
+parser.add_argument('verilog', type=str, nargs='+')
+sargs = parser.parse_args()
 
 modules = {}
+for verilog_file in sargs.verilog:
+    verilogData = open(verilog_file, "r").read()
+    verilogData = re.sub(r"//.*", "", verilogData)
+    verilogData = re.sub(r"/\*.*\*/", "", verilogData)
 
-for result in patternModule.finditer(verilogData):
-    moduleName = result.group("name")
-    moduleData = result.group("data")
-    modules[moduleName] = {
-        "args": {},
-        "params": {},
-        "data": moduleData,
-    }
-    
-    defines = []
-    
-    moduleArgs = []
-    moduleArgLast = {}
-    
-    args = []
-    for arg in result.group("args").split("\n"):
-        #print(arg)
-        if arg.startswith("`"):
-            args.append(f"{arg},")
-        else:
-            args.append(arg)
-
-    for arg in ' '.join(args).split(","):
-        moduleArg = {}
-        if not arg:
-            continue
-
-        arg = re.sub(r"\s+", " ", arg.strip())
+    for result in patternModule.finditer(verilogData):
+        moduleName = result.group("name")
+        moduleData = result.group("data")
+        modules[moduleName] = {
+            "filename": verilog_file,
+            "args": {},
+            "params": {},
+            "data": moduleData,
+            "sub": [],
+        }
         
-        if arg.startswith("`"):
-            if arg.startswith("`ifndef"):
-                defines.append(arg)
-            elif arg.startswith("`ifdef"):
-                defines.append(arg)
-            elif arg.startswith("`endif"):
-                defines.pop()
-        else:
-
-            moduleArg["defines"] = defines.copy()
-
-            argm = patternArg.search(arg)
-            if argm:
-                if argm["dir"]:
-                    moduleArg["direction"] = argm["dir"]
-                if argm["type"]:
-                    moduleArg["type"] = argm["type"]
-                if argm["size"]:
-                    moduleArg["size"] = argm["size"]
-                if argm["name"]:
-                    moduleArg["name"] = argm["name"]
-                modules[moduleName]["args"][moduleArg["name"]] = moduleArg
-                moduleArgLast = moduleArg
-            elif len(arg.strip().split()) == 1:
-                moduleArg = moduleArgLast.copy()
-                moduleArg["name"] = arg.strip()
-                modules[moduleName]["args"][moduleArg["name"]] = moduleArg
+        defines = []
+        
+        moduleArgs = []
+        moduleArgLast = {}
+        
+        args = []
+        for arg in result.group("args").split("\n"):
+            #print(arg)
+            if arg.startswith("`"):
+                args.append(f"{arg},")
             else:
-                print(f"UNKNOWN ARG ({moduleName}): {arg.strip()}")
+                args.append(arg)
 
-    if result.group("params"):
-        for arg in result.group("params").strip().lstrip("#").lstrip("(").rstrip(")").split(","):
+        for arg in ' '.join(args).split(","):
+            moduleArg = {}
+            if not arg:
+                continue
+
+            arg = re.sub(r"\s+", " ", arg.strip())
+            
+            if arg.startswith("`"):
+                if arg.startswith("`ifndef"):
+                    defines.append(arg)
+                elif arg.startswith("`ifdef"):
+                    defines.append(arg)
+                elif arg.startswith("`endif"):
+                    defines.pop()
+            else:
+
+                moduleArg["defines"] = defines.copy()
+
+                argm = patternArg.search(arg)
+                if argm:
+                    if argm["dir"]:
+                        moduleArg["direction"] = argm["dir"]
+                    if argm["type"]:
+                        moduleArg["type"] = argm["type"]
+                    if argm["size"]:
+                        moduleArg["size"] = argm["size"]
+                    if argm["name"]:
+                        moduleArg["name"] = argm["name"]
+                    modules[moduleName]["args"][moduleArg["name"]] = moduleArg
+                    moduleArgLast = moduleArg
+                elif len(arg.strip().split()) == 1:
+                    moduleArg = moduleArgLast.copy()
+                    moduleArg["name"] = arg.strip()
+                    modules[moduleName]["args"][moduleArg["name"]] = moduleArg
+                else:
+                    print(f"UNKNOWN ARG ({moduleName}): {arg.strip()}")
+
+        if result.group("params"):
+            for arg in result.group("params").strip().lstrip("#").lstrip("(").rstrip(")").split(","):
+                param = patternParam.search(arg.strip())
+                if param:
+                    moduleParam = {}
+                    moduleParam["name"] = param["name"].strip()
+                    if param["type"]:
+                        moduleParam["size"] = param["type"].strip().replace(" ", "")
+                    if param["size"]:
+                        moduleParam["size"] = param["size"].strip().replace(" ", "")
+                    if param["default"]:
+                        moduleParam["default"] = param["default"].strip().lstrip("=").strip().replace(" ", "").replace("_", "")
+
+                    modules[moduleName]["params"][moduleParam["name"]] = moduleParam
+                else:
+                    print(f"UNKNOWN PARAMETER ({moduleName}): {arg.strip()}")
+
+
+        data = re.sub(r"`(if|else|end|def).*", "", result.group("data"))
+        for arg in data.split(";"):
             param = patternParam.search(arg.strip())
             if param:
                 moduleParam = {}
@@ -98,27 +114,31 @@ for result in patternModule.finditer(verilogData):
 
                 modules[moduleName]["params"][moduleParam["name"]] = moduleParam
             else:
-                print(f"UNKNOWN PARAMETER ({moduleName}): {arg.strip()}")
 
+                known = {
+                    "if",
+                    "reg",
+                    "wire",
+                    "always",
+                    "end",
+                    "assign",
+                    "localparam",
+                    "initial",
+                    "generate",
+                    "case",
+                    "else",
+                    "endcase",
+                    "`assert",
+                    "",
+                    "(*",
+                    "{",
+                }
+                arg = arg.strip()
+                splitted = arg.split()
+                if arg and "(" in arg and splitted[0] not in known and len(splitted) > 2 and splitted[1] != "=" and splitted[1] != "<=":
+                    modules[moduleName]["sub"].append([splitted[0], arg])
 
-
-    for arg in result.group("data").split(";"):
-        param = patternParam.search(arg.strip())
-        if param:
-            moduleParam = {}
-            moduleParam["name"] = param["name"].strip()
-            if param["type"]:
-                moduleParam["size"] = param["type"].strip().replace(" ", "")
-            if param["size"]:
-                moduleParam["size"] = param["size"].strip().replace(" ", "")
-            if param["default"]:
-                moduleParam["default"] = param["default"].strip().lstrip("=").strip().replace(" ", "").replace("_", "")
-
-            modules[moduleName]["params"][moduleParam["name"]] = moduleParam
-
-
-
-#exit(0)
+    #exit(0)
 
 output = "html"
 
@@ -156,31 +176,51 @@ table, th, td {
 
     """)
 
-    for moduleName, module in modules.items():
-        print(f"<h2>{moduleName}</h2>")
-        print(f"<hr/>")
+    for verilog_file in sargs.verilog:
+        print(f"<h1>{verilog_file.split('/')[-1]}</h1>")
 
-        print("<h3>arguments</h3>")
-        print("<table>")
-        print("<tr><th>direction</th><th>type</th><th>size</th><th>name</th></tr>")
-        for argName, arg in module["args"].items():
-            print(f"<tr><td>{arg.get('direction', '')}</td><td>{arg.get('type', '')}</td><td>{arg.get('size', '')}</td><td>{argName}</td><td>{arg.get('defines') or ''}</td></tr>")
-        print("</table>")
+        for moduleName, module in modules.items():
 
-        print("<br>")
+            if module["filename"] != verilog_file:
+                continue
 
-        if module["params"]:
+            print(f"<h2 id='{moduleName}'>{moduleName}</h2>")
+            print(f"<hr/>")
+
+            print("<h3>arguments</h3>")
             print("<table>")
-            print("<h3>parameter</h3>")
-            print("<tr><th>size</th><th>name</th><th>default</th></tr>")
-            for argName, arg in module["params"].items():
-                print(f"<tr><td>{arg.get('size', '')}</td><td>{arg.get('name', '')}</td><td>{arg.get('default', '')}</td></tr>")
+            print("<tr><th>direction</th><th>type</th><th>size</th><th>name</th></tr>")
+            for argName, arg in module["args"].items():
+                print(f"<tr><td>{arg.get('direction', '')}</td><td>{arg.get('type', '')}</td><td>{arg.get('size', '')}</td><td>{argName}</td><td>{arg.get('defines') or ''}</td></tr>")
             print("</table>")
+
             print("<br>")
 
-        print("<pre>")
-        print(module["data"])
-        print("</pre>")
+            if module["params"]:
+                print("<h3>parameter</h3>")
+                print("<table>")
+                print("<tr><th>size</th><th>name</th><th>default</th></tr>")
+                for argName, arg in module["params"].items():
+                    print(f"<tr><td>{arg.get('size', '')}</td><td>{arg.get('name', '')}</td><td>{arg.get('default', '')}</td></tr>")
+                print("</table>")
+                print("<br>")
+
+            if module["sub"]:
+                print("<h3>sub modules</h3>")
+                print("<table>")
+                print("<tr><th>module</th><th>source</th></tr>")
+                for sub in module["sub"]:
+                    if sub[0] in modules:
+                        
+                        print(f"<tr><td><a href='#{sub[0]}'>{sub[0]}</a></td><td><pre>{sub[1]}</pre></td></tr>")
+
+                print("</table>")
+                print("<br>")
+
+
+            #print("<pre>")
+            #print(module["data"])
+            #print("</pre>")
 
 
 

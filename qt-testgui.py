bd1f00b004c17cceaacb13cd3a2e8e4e667667c3
Oliver Dippel <o.dippel@gmx.de>
2023-12-22 23:58:02 +0100

    adding CH341 (USB2SPI) support to qt-testgui.py to test the SPI interface

--- qt-testgui.py
+++ qt-testgui.py
@@ -4,6 +4,7 @@ import time
 from functools import partial
 from struct import *
 
+
 from PyQt5.QtCore import QDateTime, Qt, QTimer
 from PyQt5.QtWidgets import (
     QApplication,
@@ -31,11 +32,143 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+
+class CH341():
+
+    DEFAULT_TIMEOUT         = 1000     # 1000mS for USB timeouts
+    BULK_WRITE_ENDPOINT     = 0x02
+    BULK_READ_ENDPOINT      = 0x82
+
+    PACKET_LENGTH     = 0x20
+    MAX_PACKETS       = 256
+    MAX_PACKET_LEN    = (PACKET_LENGTH * MAX_PACKETS)
+    USB_VENDOR       = 0x1A86
+    USB_PRODUCT      = 0x5512
+
+    CMD_SET_OUTPUT   = 0xA1
+    CMD_IO_ADDR      = 0xA2
+    CMD_PRINT_OUT    = 0xA3
+    CMD_SPI_STREAM   = 0xA8
+    CMD_SIO_STREAM   = 0xA9
+    CMD_I2C_STREAM   = 0xAA
+    CMD_UIO_STREAM   = 0xAB
+
+    CMD_I2C_STM_STA  = 0x74
+    CMD_I2C_STM_STO  = 0x75
+    CMD_I2C_STM_OUT  = 0x80
+    CMD_I2C_STM_IN   = 0xC0
+    CMD_I2C_STM_MAX  = min(0x3F, PACKET_LENGTH)
+    CMD_I2C_STM_SET  = 0x60
+    CMD_I2C_STM_US   = 0x40
+    CMD_I2C_STM_MS   = 0x50
+    CMD_I2C_STM_DLY  = 0x0F
+    CMD_I2C_STM_END  = 0x00
+
+    CMD_UIO_STM_IN   = 0x00
+    CMD_UIO_STM_DIR  = 0x40
+    CMD_UIO_STM_OUT  = 0x80
+    CMD_UIO_STM_US   = 0xC0
+    CMD_UIO_STM_END  = 0x20
+
+    STM_I2C_20K      = 0x00
+    STM_I2C_100K     = 0x01
+    STM_I2C_400K     = 0x02
+    STM_I2C_750K     = 0x03
+    STM_SPI_DBL      = 0x04
+
+    reverse_table = [
+        0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0,
+        0x10, 0x90, 0x50, 0xd0, 0x30, 0xb0, 0x70, 0xf0,
+        0x08, 0x88, 0x48, 0xc8, 0x28, 0xa8, 0x68, 0xe8,
+        0x18, 0x98, 0x58, 0xd8, 0x38, 0xb8, 0x78, 0xf8,
+        0x04, 0x84, 0x44, 0xc4, 0x24, 0xa4, 0x64, 0xe4,
+        0x14, 0x94, 0x54, 0xd4, 0x34, 0xb4, 0x74, 0xf4,
+        0x0c, 0x8c, 0x4c, 0xcc, 0x2c, 0xac, 0x6c, 0xec,
+        0x1c, 0x9c, 0x5c, 0xdc, 0x3c, 0xbc, 0x7c, 0xfc,
+        0x02, 0x82, 0x42, 0xc2, 0x22, 0xa2, 0x62, 0xe2,
+        0x12, 0x92, 0x52, 0xd2, 0x32, 0xb2, 0x72, 0xf2,
+        0x0a, 0x8a, 0x4a, 0xca, 0x2a, 0xaa, 0x6a, 0xea,
+        0x1a, 0x9a, 0x5a, 0xda, 0x3a, 0xba, 0x7a, 0xfa,
+        0x06, 0x86, 0x46, 0xc6, 0x26, 0xa6, 0x66, 0xe6,
+        0x16, 0x96, 0x56, 0xd6, 0x36, 0xb6, 0x76, 0xf6,
+        0x0e, 0x8e, 0x4e, 0xce, 0x2e, 0xae, 0x6e, 0xee,
+        0x1e, 0x9e, 0x5e, 0xde, 0x3e, 0xbe, 0x7e, 0xfe,
+        0x01, 0x81, 0x41, 0xc1, 0x21, 0xa1, 0x61, 0xe1,
+        0x11, 0x91, 0x51, 0xd1, 0x31, 0xb1, 0x71, 0xf1,
+        0x09, 0x89, 0x49, 0xc9, 0x29, 0xa9, 0x69, 0xe9,
+        0x19, 0x99, 0x59, 0xd9, 0x39, 0xb9, 0x79, 0xf9,
+        0x05, 0x85, 0x45, 0xc5, 0x25, 0xa5, 0x65, 0xe5,
+        0x15, 0x95, 0x55, 0xd5, 0x35, 0xb5, 0x75, 0xf5,
+        0x0d, 0x8d, 0x4d, 0xcd, 0x2d, 0xad, 0x6d, 0xed,
+        0x1d, 0x9d, 0x5d, 0xdd, 0x3d, 0xbd, 0x7d, 0xfd,
+        0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3,
+        0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3,
+        0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb,
+        0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb,
+        0x07, 0x87, 0x47, 0xc7, 0x27, 0xa7, 0x67, 0xe7,
+        0x17, 0x97, 0x57, 0xd7, 0x37, 0xb7, 0x77, 0xf7,
+        0x0f, 0x8f, 0x4f, 0xcf, 0x2f, 0xaf, 0x6f, 0xef,
+        0x1f, 0x9f, 0x5f, 0xdf, 0x3f, 0xbf, 0x7f, 0xff
+    ]
+
+    def __init__(self, vid=USB_VENDOR, pid=USB_PRODUCT):
+        dev = usb.core.find(idVendor=vid, idProduct=pid)
+
+        if dev is None:
+            raise ConnectionError("Device not found (%x:%x)" % (vid, pid))
+        print(f'Found CH341 device ({vid:x}:{pid:x})')
+        if (dev.bNumConfigurations != 1):
+            raise ConnectionError("Device configuration error")
+        dev.set_configuration()
+        self.dev = dev
+
+
+        #cmd = [self.CMD_I2C_STREAM, self.CMD_UIO_STM_US | 100, self.CMD_UIO_STM_END]
+        #cnt = self.dev.write(self.BULK_WRITE_ENDPOINT, cmd)
+        #if (cnt != len(cmd)):
+        #    raise ConnectionError("Failed to issue Command")
+
+        cmd = [self.CMD_UIO_STREAM, self.CMD_UIO_STM_DIR | 0x3F, self.CMD_UIO_STM_END]
+        cnt = self.dev.write(self.BULK_WRITE_ENDPOINT, cmd)
+        if (cnt != len(cmd)):
+            raise ConnectionError("Failed to issue dir command")
+
+
+    def spi_trans(self, data):
+        cmd = [self.CMD_UIO_STREAM, self.CMD_UIO_STM_OUT | 0x36, self.CMD_UIO_STM_END]
+        cnt = self.dev.write(self.BULK_WRITE_ENDPOINT, cmd)
+        if (cnt != len(cmd)):
+            raise ConnectionError("Failed to issue select cs")
+
+        cmd = [self.CMD_SPI_STREAM]
+        for c in data:
+            cmd.append(self.reverse_table[c])
+
+        cnt = self.dev.write(self.BULK_WRITE_ENDPOINT, cmd)
+        #if (cnt != len(cmd)):
+        #    raise ConnectionError("Failed to write data")
+
+        data = self.dev.read(self.BULK_READ_ENDPOINT, cnt-1)
+
+        cmd = [self.CMD_UIO_STREAM, self.CMD_UIO_STM_OUT | 0x37, self.CMD_UIO_STM_END]
+        cnt = self.dev.write(self.BULK_WRITE_ENDPOINT, cmd)
+        if (cnt != len(cmd)):
+            raise ConnectionError("Failed to issue unselect cs")
+
+        ret = []
+        for c in data:
+            ret.append(self.reverse_table[c])
+
+        return list(ret)
+
+
+
 project = projectLoader.load(args.json)
 
 SHM_FILE = ""
 SERIAL = ""
 NET_IP = ""
+USB = None
 if args.device and args.device.startswith("/dev/tty"):
     import serial
 
@@ -44,6 +177,10 @@ if args.device and args.device.startswith("/dev/tty"):
     ser = serial.Serial(SERIAL, BAUD, timeout=0.001)
 elif args.device and args.device.startswith("/dev/shm/"):
     SHM_FILE = args.device
+elif args.device and args.device.startswith("USB"):
+    import usb.core
+    import usb.util
+    USB = CH341()
 elif args.device and args.device != "":
     NET_IP = args.device
     NET_PORT = args.port
@@ -59,7 +196,6 @@ elif args.device and args.device != "":
     except:
         pass
 
-
 else:
     import spidev
 
@@ -72,6 +208,7 @@ else:
     spi.lsbfirst = False
 
 INTERVAL = 50
+
 
 data = [0] * (project["data_size"] // 8)
 data[0] = 0x74
@@ -858,6 +995,11 @@ class WinForm(QWidget):
                 ser.write(bytes(data))
                 msgFromServer = ser.read(len(data))
                 rec = list(msgFromServer)
+
+            elif USB is not None:
+                print("USB")
+                rec = USB.spi_trans(data)
+
             elif SHM_FILE:
                 fd = open(f"{SHM_FILE}.tx", "wb")
                 fd.write(bytes(data))
